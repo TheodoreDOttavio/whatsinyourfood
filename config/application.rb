d0a9c597e5b7b478d9514118e27282d93470ee3f
theodoredottavio <teddottavio@yahoo.com>
2016-07-05 06:23:32 -0400

    yup local is pointless set compile in production and action dispatch

--- config/application.rb
+++ config/application.rb
@@ -32,7 +32,7 @@ module Whatsinyourfood
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     
-    config.serve_static_files = true
+    #config.serve_static_files = true
     config.assets.precompile += %w( style.scss )
   end
 end
