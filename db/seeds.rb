3eda77cba4a25b39259589a5950b84278ca9e935
theodoredottavio <teddottavio@yahoo.com>
2016-07-14 16:03:27 -0400

    ... and the questions

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,23 +12,23 @@ Topic.create!(question: "Which product has the most calories from fat?",
                  qtype: true )
 Topic.create!(question: "Which product has the highest total fat content?",
                  statement: "The highest total fat content:",
-                 test_field: "nf_total_fat_percent",
+                 test_field: "nf_total_fat_pergram",
                  qtype: true)
 Topic.create!(question: "Which product has the most saturated fat?",
                  statement: "The most saturated fat:",
-                 test_field: "nf_saturated_fat_percent",
+                 test_field: "nf_saturated_fat_pergram",
                  qtype: true)
 Topic.create!(question: "Which product has the highest amount of cholesterol?",
                  statement: "The highest amount of cholesterol:",
-                 test_field: "nf_cholesterol_percent",
+                 test_field: "nf_cholesterol_pergram",
                  qtype: true)
 Topic.create!(question: "Which product has the most salt?",
                  statement: "The most salt:",
-                 test_field: "nf_sodium_percent",
+                 test_field: "nf_sodium_pergram",
                  qtype: true)
 Topic.create!(question: "Which product has the most protein?",
                  statement: "The most protein:",
-                 test_field: "nf_protein_percent",
+                 test_field: "nf_protein_pergram",
                  qtype: true)
 Topic.create!(question: "Which product has the most vitamin A?",
                  statement: "The most vitamin A:",
@@ -44,7 +44,7 @@ Topic.create!(question: "Which product has the most iron?",
                  qtype: true)
 Topic.create!(question: "Which product has the most total carbohydrates?",
                  statement: "The most total carbohydrates:",
-                 test_field: "nf_total_carbohydrate_percent",
+                 test_field: "nf_total_carbohydrate_pergram",
                  qtype: true)
 Topic.create!(question: "Which product has the most vitamin C?",
                  statement: "The most vitamin C:",
@@ -61,23 +61,23 @@ Topic.create!(question: "Which product has the least calories from fat?",
                  qtype: false)
 Topic.create!(question: "Which product has the lowest total fat content?",
                  statement: "The lowest total fat content:",
-                 test_field: "nf_total_fat_percent",
+                 test_field: "nf_total_fat_pergram",
                  qtype: false)
 Topic.create!(question: "Which product has the least saturated fat?",
                  statement: "The least saturated fat:",
-                 test_field: "nf_saturated_fat_percent",
+                 test_field: "nf_saturated_fat_pergram",
                  qtype: false)
 Topic.create!(question: "Which product has the lowest amount of cholesterol?",
                  statement: "The lowest amount of cholesterol:",
-                 test_field: "nf_cholesterol_percent",
+                 test_field: "nf_cholesterol_pergram",
                  qtype: false)
 Topic.create!(question: "Which product has the least salt?",
                  statement: "The least salt:",
-                 test_field: "nf_sodium_percent",
+                 test_field: "nf_sodium_pergram",
                  qtype: false)
 Topic.create!(question: "Which product has the least protein?",
                  statement: "The least protein:",
-                 test_field: "nf_protein_percent",
+                 test_field: "nf_protein_pergram",
                  qtype: false)
 Topic.create!(question: "Which product has the least vitamin A?",
                  statement: "The least vitamin A:",
@@ -93,7 +93,7 @@ Topic.create!(question: "Which product has the least iron?",
                  qtype: false)
 Topic.create!(question: "Which product has the least total carbohydrates?",
                  statement: "The least total carbohydrates:",
-                 test_field: "nf_total_carbohydrate_percent",
+                 test_field: "nf_total_carbohydrate_pergram",
                  qtype: false)
 Topic.create!(question: "Which product has the least vitamin C?",
                  statement: "The least vitamin C:",
