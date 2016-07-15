89c253bbd4a10221c58f5d7de9367cec058f54c7
theodoredottavio <teddottavio@yahoo.com>
2016-07-15 02:40:23 -0400

    preventing less than five questions

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,7 +5,8 @@ class Product < ActiveRecord::Base
   scope :allbrands, ->(myfield) { select(:brand_name).where("#{myfield} >= 1").distinct.pluck(:brand_name) }
   scope :brandcount, ->(brand) { where(brand_name: brand).count }
 
-  scope :random, ->(myfield) { select("#{myfield} as test_condition, *").where("#{myfield} > 0").offset(rand(Product.count)).limit(1) }
+  scope :totalavailable, ->(myfield) { where("#{myfield} > 0").count }
+  scope :random, ->(myfield) { select("#{myfield} as test_condition, *").where("#{myfield} > 0").offset(rand(Product.totalavailable(myfield))).limit(1) }
 
   scope :tester, ->(brand, offsets, myfield) { select("#{myfield} as test_condition, *").where(brand_name: brand).where("#{myfield} >= 1").offset(offsets).limit(1) }
 end
