e082285b38c0fc49c2fccbb982191f9e466ec902
theodoredottavio <teddottavio@yahoo.com>
2016-07-15 03:30:21 -0400

    reduced more DB calls and simplified calculating pergream data

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -18,51 +18,45 @@ module ApplicationHelper
 
   def gramstopergram(product_id)
     #nf_serving_weight_grams
+    fieldlist = {
+      :nf_calories_from_fat_pergram => "nf_calories_from_fat",
+      :nf_calories_pergram => "nf_calories",
+      :nf_total_fat_pergram => "nf_total_fat",
+      :nf_saturated_fat_pergram => "nf_saturated_fat",
+      :nf_trans_fatty_acid_pergram => "nf_trans_fatty_acid",
+      :nf_polyunsaturated_fat_pergram => "nf_polyunsaturated_fat",
+      :nf_monounsaturated_fat_pergram => "nf_monounsaturated_fat",
+      :nf_cholesterol_pergram => "nf_cholesterol",
+      :nf_total_carbohydrate_pergram => "nf_total_carbohydrate",
+      :nf_dietary_fiber_pergram => "nf_dietary_fiber",
+      :nf_sugars_pergram => "nf_sugars",
+      :nf_protein_pergram => "nf_protein",
+      :nf_vitamin_a_dv_pergram => "nf_vitamin_a_dv",
+      :nf_vitamin_c_dv_pergram => "nf_vitamin_c_dv",
+      :nf_calcium_dv_pergram => "nf_calcium_dv",
+      :nf_iron_dv_pergram => "nf_iron_dv",
+      :nf_potassium_pergram => "nf_potassium"
+    }
+
     obj = Product.find(product_id)
     if obj['nf_serving_weight_grams'].to_i != 0 then
       mysize = obj['nf_serving_weight_grams'] + 0.000
 
-      obj.update(:nf_calories_from_fat_pergram => obj['nf_calories_from_fat'].to_i/mysize) if obj['nf_calories_from_fat'].to_i != 0
-      obj.update(:nf_calories_pergram => obj['nf_calories'].to_i/mysize ) if obj['nf_calories'].to_i != 0
-      obj.update(:nf_total_fat_pergram => obj['nf_total_fat'].to_i/mysize) if obj['nf_total_fat'].to_i != 0
-      obj.update(:nf_saturated_fat_pergram => obj['nf_saturated_fat'].to_i/mysize) if obj['nf_saturated_fat'].to_i != 0
-      obj.update(:nf_trans_fatty_acid_pergram => obj['nf_trans_fatty_acid'].to_i/mysize) if obj['nf_trans_fatty_acid'].to_i != 0
-      obj.update(:nf_polyunsaturated_fat_pergram => obj['nf_polyunsaturated_fat'].to_i/mysize) if obj['nf_polyunsaturated_fat'].to_i != 0
-      obj.update(:nf_monounsaturated_fat_pergram => obj['nf_monounsaturated_fat'].to_i/mysize) if obj['nf_monounsaturated_fat'].to_i != 0
-      obj.update(:nf_cholesterol_pergram => obj['nf_cholesterol'].to_i/mysize) if obj['nf_cholesterol'].to_i != 0
-      obj.update(:nf_sodium_pergram => (obj['nf_sodium'].to_i/1000.000)/mysize) if obj['nf_sodium'].to_i != 0
-      obj.update(:nf_total_carbohydrate_pergram => obj['nf_total_carbohydrate'].to_i/mysize) if obj['nf_total_carbohydrate'].to_i != 0
-      obj.update(:nf_dietary_fiber_pergram => obj['nf_dietary_fiber'].to_i/mysize) if obj['nf_dietary_fiber'].to_i != 0
-      obj.update(:nf_sugars_pergram => obj['nf_sugars'].to_i/mysize) if obj['nf_sugars'].to_i != 0
-      obj.update(:nf_protein_pergram => obj['nf_protein'].to_i/mysize) if obj['nf_protein'].to_i != 0
-      obj.update(:nf_vitamin_a_dv_pergram => obj['nf_vitamin_a_dv'].to_i/mysize) if obj['nf_vitamin_a_dv'].to_i != 0
-      obj.update(:nf_vitamin_c_dv_pergram => obj['nf_vitamin_c_dv'].to_i/mysize) if obj['nf_vitamin_c_dv'].to_i != 0
-      obj.update(:nf_calcium_dv_pergram => obj['nf_calcium_dv'].to_i/mysize) if obj['nf_calcium_dv'].to_i != 0
-      obj.update(:nf_iron_dv_pergram => obj['nf_iron_dv'].to_i/mysize) if obj['nf_iron_dv'].to_i != 0
-      obj.update(:nf_potassium_pergram => obj['nf_potassium'].to_i/mysize) if obj['nf_potassium'].to_i != 0
+      fieldlist.each do |fkey, fvalue|
+        if obj[fvalue].to_i != 0 then
+          storevalue = obj[fvalue].to_i/mysize
+          storevalue = storevalue.round(4)
+          obj.update(fkey => storevalue)
+        end
+      end
 
-      obj.update(:nf_calories_from_fat_pergram => obj['nf_calories_from_fat_pergram'].round(3))
-      obj.update(:nf_calories_pergram => obj['nf_calories_pergram'].round(3))
-      obj.update(:nf_total_fat_pergram => obj['nf_total_fat_pergram'].round(3))
-      obj.update(:nf_saturated_fat_pergram => obj['nf_saturated_fat_pergram'].round(3))
-      obj.update(:nf_trans_fatty_acid_pergram => obj['nf_trans_fatty_acid_pergram'].round(3))
-      obj.update(:nf_polyunsaturated_fat_pergram => obj['nf_polyunsaturated_fat_pergram'].round(3))
-      obj.update(:nf_monounsaturated_fat_pergram => obj['nf_monounsaturated_fat_pergram'].round(3))
-      obj.update(:nf_cholesterol_pergram => obj['nf_cholesterol_pergram'].round(3))
-      obj.update(:nf_sodium_pergram => obj['nf_sodium_pergram'].round(5))
-      obj.update(:nf_total_carbohydrate_pergram => obj['nf_total_carbohydrate_pergram'].round(3))
-      obj.update(:nf_dietary_fiber_pergram => obj['nf_dietary_fiber_pergram'].round(3))
-      obj.update(:nf_sugars_pergram => obj['nf_sugars_pergram'].round(3))
-      obj.update(:nf_protein_pergram => obj['nf_protein_pergram'].round(3))
-      obj.update(:nf_vitamin_a_dv_pergram => obj['nf_vitamin_a_dv_pergram'].round(3))
-      obj.update(:nf_vitamin_c_dv_pergram => obj['nf_vitamin_c_dv_pergram'].round(3))
-      obj.update(:nf_calcium_dv_pergram => obj['nf_calcium_dv_pergram'].round(3))
-      obj.update(:nf_iron_dv_pergram => obj['nf_iron_dv_pergram'].round(3))
-      obj.update(:nf_potassium_pergram => obj['nf_potassium_pergram'].round(3))
+      #salt is in milligrams .. convert it to grams and extend it to 6 decimals instead of 4
+      obj.update(:nf_sodium_pergram => (obj['nf_sodium'].to_i/1000.000)/mysize) if obj['nf_sodium'].to_i != 0
+      obj.update(:nf_sodium_pergram => obj['nf_sodium_pergram'].round(6))
 
       obj.save(:validate => false)
     else
-      #trash it...
+      #trash it bcause there is no serving size in grams!
       puts "deleting product id " + product_id.to_s
       Product.destroy(product_id)
     end
