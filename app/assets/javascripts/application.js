700cd3846732ff33f1d15bb1db88ef327d5593ec
theodoredottavio <teddottavio@yahoo.com>
2016-07-19 16:32:00 -0400

    laying out mobile app CSS

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,20 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '260554340991212',
+      xfbml      : true,
+      version    : 'v2.7'
+    });
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+</script>
