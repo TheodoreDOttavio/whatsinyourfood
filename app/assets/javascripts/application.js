8aa51ac29ab142d455ede58ce0f6318b76b907be
theodoredottavio <teddottavio@yahoo.com>
2016-07-23 13:56:50 -0400

    moving fb js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,19 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '260554340991212',
+      xfbml      : true,
+      version    : 'v2.7'
+    });
+  };
+
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.7&appId=260554340991212";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
