e9e858e143b11fd54d37bbe1eccebbe07b601de3
teddottavio <teddottavio@yahoo.com>
2017-06-16 13:17:46 -0400

    changed is not to <> for postgre

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,5 +6,5 @@ class Product < ActiveRecord::Base
   #based on range of values between 0-1 of pergram, these scopes find the 30% and 60% values
   scope :pergrammax, ->(myfield) {select("MAX(#{myfield}) as top_value")}
 
-  scope :random, ->(myfield, mn=0.0001, mx=1000, selectedids=1) { select("#{myfield} as test_condition, *").where("#{myfield} >= #{mn} and #{myfield} <= #{mx} and id is not #{selectedids}").offset(rand(Product.totalavailable(myfield, mn, mx))).limit(1) }
+  scope :random, ->(myfield, mn=0.0001, mx=1000, selectedids=1) { select("#{myfield} as test_condition, *").where("#{myfield} >= #{mn} and #{myfield} <= #{mx} and id <> #{selectedids}").offset(rand(Product.totalavailable(myfield, mn, mx))).limit(1) }
 end
